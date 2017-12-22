45ea49143e53ccb364e86618551c1424be45d18b
Carlem <fifi-44@o2.pl>
2017-12-22 14:59:07 +0100

    v2.0

--- build_gcp.sh
+++ build_gcp.sh
@@ -5,16 +5,24 @@ project_id="$1"
 deploy_environment="$2"
 git_sha1="$3"
 cluster_name="hello-cluster"
+service_name="hello-web"
+
 tag="gcr.io/${project_id}/docker-helloworld:${git_sha1}.${deploy_environment}.v${BUILD_NUMBER}"
 
 #create kubernetes cluster "hello-cluster" with 3 nodes
-gcloud container clusters create ${cluster_name} --num-nodes=3
+gcloud container clusters create ${cluster_name} \
+  --num-nodes=3
 #login to cluster
 gcloud container clusters get-credentials ${cluster_name}
 #run helloworld from google repo
-kubectl run hello-web --image=$tag --port 8080
-
-#run LoadBalancer with gate to internet
-kubectl expose deployment hello-web --type=LoadBalancer --port 80 --target-port 8080
+kubectl run ${service_name} \
+  --image=$tag \
+  --port 8080
+#run LoadBalancer
+kubectl expose deployment ${service_name} \
+  --type=LoadBalancer \
+  --port 80 \
+  --target-port 8080
 #run in scale
-kubectl scale deployment hello-web --replicas=3
+kubectl scale deployment ${service_name} \
+  --replicas=3
---
+++ destory_gcp.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e -u
+# ------- clean-up
+cluster_name="hello-cluster"
+service_name="hello-web"
+
+kubectl delete service ${service_name}
+gcloud compute forwarding-rules list
+
+##WAIT 30sec
+sleep 30
+
+gcloud container clusters delete ${cluster_name}
