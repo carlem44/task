1305daaa7e0cdec40273b15f4e74606a77267647
Carlem <fifi-44@o2.pl>
2017-12-22 15:09:41 +0100

    v2.3

--- destory_gcp.sh
+++ destory_gcp.sh
@@ -5,9 +5,9 @@ cluster_name="hello-cluster"
 service_name="hello-web"
 
 kubectl delete service ${service_name}
-gcloud compute forwarding-rules list
 
-##WAIT 30sec
+#gcloud compute forwarding-rules list
+#WAIT 30sec
 sleep 30
 
 gcloud container clusters delete ${cluster_name}
