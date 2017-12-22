edf68cc2848097b67117deb48d7e41113a3cc92f
Carlem <fifi-44@o2.pl>
2017-12-22 14:03:05 +0100

    v1.1

--- deploy.sh
+++ deploy.sh
@@ -3,12 +3,11 @@ set -e -u
 
 project_id="$1"
 deploy_environment="$2"
-git_sha1="$3"
-tag="gcr.io/${project_id}/docker-helloworld:${git_sha1}.${deploy_environment}.v${BUILD_NUMBER}"
+tag="gcr.io/${project_id}/docker-helloworld:${deploy_environment}.v${BUILD_NUMBER}"
 
 sudo gcloud docker push "$tag"
 
 kubectl patch deployment docker-helloworld \
   --namespace=pastely-"${deploy_environment}" \
   -p \
-  '{"spec":{"template":{"spec":{"containers":[{"name":"pastely-backend","image":'"\"${tag}\""'}]}}}}'
+  '{"spec":{"template":{"spec":{"containers":[{"name":"docker-helloworld","image":'"\"${tag}\""'}]}}}}'
